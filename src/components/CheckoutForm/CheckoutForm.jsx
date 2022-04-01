3315d43038d5d1322ae31ab6602b73880cf1ba66
Cesarmnd <cesarn.davila@gmail.com>
2022-04-01 00:41:37 -0500

    ACtualizacion de desafio de firebase ii

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,4 +1,5 @@
 import { useState} from 'react';
+import './checkout.css';
 import { useCartContext } from '../../context/CartContext';
 import { addDoc, collection, documentId, getDocs, getFirestore, query, where, writeBatch } from 'firebase/firestore'
 
@@ -27,9 +28,10 @@ function CheckoutForm() {
     const queryCollectionSet = collection(db, 'orders')
     addDoc(queryCollectionSet, list)
     
-    .then(resp => console.log(resp))
+    .then(function(docRef) {
+      alert(`Thanks for buying in our store! Your id transaction is: "${docRef.id}". We're going to send you an email with more information.`, );
+      })
     .catch(err => console.log(err))
-    .finally(()=> console.log('finished'))
     
 
     const queryCollection = collection(db, 'items')
@@ -45,15 +47,18 @@ function CheckoutForm() {
     })))
     batch.commit()
   }
-console.log(cartList)
 
   return (
-    <form>
-      <input type="text" name='name' value={dataForm.name} onChange={handleOnChange} required/>
-      <input type="email" name='email' value={dataForm.email} onChange={handleOnChange} required/>
-      <input type="number" name='number' value={dataForm.number} onChange={handleOnChange} required/>
-      <button className="btn" onClick={checkout}>Checkout</button>
-    </form>
+    <div className='form'>
+      <h1 className='form__title'>Checkout Form</h1>
+      <p className='form__description'>Please fill out all the camps with your information:</p>
+      <form className='form__container'>
+        <label htmlFor="name">Name:</label><input type="text" name='name'  id="name"  value={dataForm.name} onChange={handleOnChange} required/>
+        <label htmlFor="email">Email:</label><input type="email" name='email'  id="email"  value={dataForm.email} onChange={handleOnChange} required/>
+        <label htmlFor="number">Phone number:</label><input type="number" name='number'  id="number"  value={dataForm.number} onChange={handleOnChange} required/>
+        <button className="btn btn-form" onClick={checkout}>Checkout</button>
+      </form>
+    </div>
   )
 }
 
